26bf9528f55aa5daeb83497d8c5e3f9a337b2781
Supriyo Mahanta <mahantasupriyo@gmail.com>
2020-06-10 00:41:21 +0530

    fix bug in navbar and footer added

--- js/script.js
+++ js/script.js
@@ -36,6 +36,7 @@ function filterFunction() {
 
 $(document).ready(function () {
   // executes when HTML-Document is loaded and DOM is ready
+
   // breakpoint and up
   $(window).resize(function () {
     if ($(window).width() >= 980) {
@@ -44,12 +45,15 @@ $(document).ready(function () {
         $(this).parent().toggleClass("show");
         $(this).parent().find(".dropdown-menu").toggleClass("show");
       });
+
       // hide the menu when the mouse leaves the dropdown
       $(".navbar .dropdown-menu").mouseleave(function () {
         $(this).removeClass("show");
       });
+
       // do something here
     }
   });
+
   // document ready
 });
